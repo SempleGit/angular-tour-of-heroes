fcabbe3037cbea4b84754b072f9d34c2cd24416c
Daniel Semple <dsemple.isu@gmail.com>
2021-06-17 13:04:25 -0500

    Started routing section, added route to heroes, link added to the /heroes site. Updated the app-component css as document in the tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule
   ],
